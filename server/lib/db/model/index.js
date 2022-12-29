65845d1f456203b6171df2079bbb6713cafdffee
sanggyeol <sanggyeol1@gmail.com>
2022-12-30 00:06:15 +0900

    로그인 기능구현 및 bd 사용법 배움

---
+++ server/lib/db/model/index.js
@@ -0,0 +1,16 @@
+const user = require('./user');
+// const board = require('./board');
+// const comment = require('./comment');
+
+const modelList = [
+    'user',
+    // 'board','comment'
+]
+
+module.exports = {
+    modelDefines : {
+        user, 
+        // board, comment
+    },
+    modelList
+}
