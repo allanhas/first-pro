3cfd95517a3fb429ec92781cd9d50faeee539c09
sanggyeol <sanggyeol1@gmail.com>
2023-01-07 21:26:22 +0900

    no message

--- front/signUp.js
+++ front/signUp.js
@@ -1,25 +1,58 @@
+//회원가입기능
+
 function clickEvent(){
-    const id=document.getElementById('id').value;
-    const pw=document.getElementById('pw').value;
-    
-    axios.post("http://localhost:3000/signUp",{
-      id:id,
-      pw:pw
-    }).then((res)=>{
-      if(res.data.data==='idFailed'){
-          console.log('아이디 실패')
-          return;
-      }
-      if(res.data.data==='pwFailed'){
-          console.log('비밀번호 실패')
-          return;
-      }
-      else{
-          console.log('성공')
-          return;
-      }
-    }).catch((err)=>{
-      console.log(err)
-    })
+  const id=document.getElementById('id').value;
+  const pw=document.getElementById('pw').value;
+  const name=document.getElementById('name').value;
+  const sex=document.getElementById('sex').value;
+  const email=document.getElementById('email').value;
+  
+  let sex_code
+  if (sex==="남자"){
+    sex_code = 1
   }
+  if (sex==="여자"){
+    sex_code = 2
+  }
+
+  axios.post("http://localhost:3000/signUp",{
+    id:id,
+    pw:pw,
+    name:name,
+    sex:sex_code,
+    email:email,
+  }).then((res)=>{
+    console.log(res.data)
+    alert(res.data.message)
+    
+    
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
